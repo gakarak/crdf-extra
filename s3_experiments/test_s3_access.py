bac3d6bb27265c4ff2eba1969746f332e872eab5
Alexander Kalinovsky (UIIP, arpc) <gakarak@gmail.com>
2015-08-05 00:42:18 +0300

    python examples for amazon-s3

--- s3_experiments/test_s3_access.py
+++ s3_experiments/test_s3_access.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python
 __author__ = 'ar'
+
+import sys
+import boto3
+
+if __name__=='__main__':
+    s3 = boto3.resource('s3')
+    for bb in  s3.buckets.all():
+        print (bb.name)
--- s3_experiments/test_s3_read_image.py
+++ s3_experiments/test_s3_read_image.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python
 __author__ = 'ar'
+
+import sys
+import cv2
+import numpy as np
+import boto3
+
+def getImageFromS3Object(s3Object):
+    img_array = np.asarray(bytearray(s3Object['Body'].read()), dtype=np.uint8)
+    ret=cv2.imdecode(img_array, cv2.CV_LOAD_IMAGE_UNCHANGED)
+    return ret
+
+########################
+if __name__=='__main__':
+    s3=boto3.resource('s3')
+    s3b=s3.Bucket('imlab')
+    for oo in s3b.objects.all():
+        print(oo)
+        o1=s3b.Object(oo.key).get()
+        winName="win"+oo.key
+        try:
+            cv2.imshow(winName, getImageFromS3Object(o1))
+        except:
+            pass
+    cv2.waitKey()
